176c1f16e1f8bf1bbc572667304b8e8454290c59
francoojoel <alfredodaniel234@gmail.com>
2022-08-17 22:57:11 -0300

    desafio1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Desafio 1, crear el CLI
         </a>
       </header>
     </div>
