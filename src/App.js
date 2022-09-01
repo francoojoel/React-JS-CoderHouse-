eea1d3ce6ba9b97c8d5444379c7a67377e7a62fb
francoojoel <alfredodaniel234@gmail.com>
2022-08-31 23:39:58 -0300

    Desafio4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './index.css';
 import './App.css';
 import Header from './components/Header';
 import News from './components/News';
+import ItemCount from './components/ItemCount';
 
 function App () {
    return (
       <div className='App-container'>
          <Header />
          <News />
+         <ItemCount initial={0} stock={10} />
       </div>
    );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({initial, stock, onAdd}) => {
+   const [count, setCounter] = useState(initial);
+
+   const sumar = () => setCounter(count + 1);
+   const restar = () => setCounter(count - 1);
+   
+  return (
+    <div className='Count-container'>
+        <button className='Counter-button' disabled={count >= stock} onClick={sumar}>+</button>
+        <h3 className='Counter'>{count}</h3>
+        <button className='Counter-button' disabled={count <= initial} onClick={restar}>-</button>
+        <div className='Add-cart-container'>
+            <button className='Cart-button' disabled={count <= 0} onClick={onAdd}>Agregar al carrito</button>
+        </div>
+    </div>
+  )
+}
+
+export default ItemCount
